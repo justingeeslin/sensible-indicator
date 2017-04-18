7bfb817fa6efccdb3b44d08038660427e7007ecb
Justin Geeslin <justin@geesl.in>
2017-04-18 15:04:33 -0500

    Remvoing General Segment code for Expand Collapse.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-// Create a list of Conversation Segment pairs. [ {key, url, title, timeStart, timeEnd, [{interest, score}]}]
-const Segment = require('./js/Segment.js')
-var myFirstSegment = new Segment();
+window.sensible = typeof sensible !== "undefined" ? sensible : {};
+sensible.classes = typeof sensible.classes !== "undefined" ? sensible.classes : {};
+
+sensible.classes.ExpandCollapse = require('./js/sensibleExpandCollapse.js')
