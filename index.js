634a5600aef44360f292e4d3a1baed6e43cb0e02
Justin Geeslin <justin@geesl.in>
2017-04-20 14:30:23 -0500

    Trying both global namespace and module exports. Makes sense right?

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 window.sensible = typeof sensible !== "undefined" ? sensible : {};
 sensible.classes = typeof sensible.classes !== "undefined" ? sensible.classes : {};
 
-sensible.classes.ExpandCollapse = require('./js/sensibleExpandCollapse.js')
+sensible.classes.ExpandCollapse = require('./js/sensibleExpandCollapse.js');
+
+module.exports = sensible.classes.ExpandCollapse;
